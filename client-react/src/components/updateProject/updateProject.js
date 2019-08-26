79e74114874b3664d509dcba2144c02a6afe4166
AirbenderProductions <mtmusicandart@gmail.com>
2019-08-26 09:49:17 -0600

    Working, cleaned up ready for pull request

---
+++ client-react/src/components/updateProject/updateProject.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './updateProject.css';
+export class UpdateProject extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Update Project Component</h3>
+            </div>
+        )
+    }
+}
+
+export default UpdateProject
