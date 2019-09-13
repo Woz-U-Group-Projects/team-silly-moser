3fee6d4791c0a6ea31ad4c7da53a8765ddeaabea
AirbenderProductions <mtmusicandart@gmail.com>
2019-09-13 13:47:25 -0600

    New changes
    new changes

--- server-express-mongodb/app.js
+++ server-express-mongodb/app.js
@@ -3,7 +3,7 @@ var exphbs  = require('express-handlebars');
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-// var cors = require("cors");
+var cors = require("cors");
 var mongoose = require("mongoose");
 const passport = require("passport");
 
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-// app.use(cors());
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
